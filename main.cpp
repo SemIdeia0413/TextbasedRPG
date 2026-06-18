5fc941ab5f16db8caa85fba64e11b98c5053a9b2
SemIdeia0413 <121650417+SemIdeia0413@users.noreply.github.com>
2026-06-18 12:56:17 +0000

    Adding a json library to the project. This will allow us to easily read and write JSON files, which can be useful for saving game state, configurations, or other data in a structured format.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 int main() {
     std::cout << "TextbasedRPG starting..." << std::endl;
-    
+    std::cout << 
 
 
     return 0;
